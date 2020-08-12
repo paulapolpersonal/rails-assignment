cfe6cd0318a9ca2f39260934f166645082749117
paulapol <paula.pol98@yahoo.ro>
2020-08-12 12:00:51 +0300

    add edit user page, add remember me functionality and some code reformatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
    get 'sessions/new'
-   root 'home#index'
+   root 'products#index'
    get '/signup', to: 'users#new'
    get '/login', to: 'sessions#new'
+   get '/edit', to: 'users#edit'
    post '/login', to: 'sessions#create'
    delete '/logout', to: 'sessions#destroy'
    resources :users
