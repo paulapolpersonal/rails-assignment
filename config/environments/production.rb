713f51154321c81b0b1c2db26776435bb83641b1
paulapol <paula.pol98@yahoo.ro>
2020-08-19 13:09:19 +0300

    modify production configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,13 +112,13 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.raise_delivery_errors = true
+  host = 'https://peaceful-journey-80259.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
   config.action_mailer.delivery_method = :smtp
-  host = 'https://peaceful-journey-80259.herokuapp.com/'
-  config.action_mailer.default_url_options = {host: host}
   ActionMailer::Base.smtp_settings = {
-      user_name: ENV['SENDGRID_USERNAMEE'],
+      user_name: ENV['SENDGRID_USERNAME'],
       password: ENV['SENDGRID_PASSWORD'],
-      domain: ENV['SENDGRID_DOMAIN'],
+      domain: 'peaceful-journey-80259.herokuapp.com',
       address: 'smtp.sendgrid.net',
       port: 587,
       authentication: :plain,
