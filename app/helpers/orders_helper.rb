999d9b1ad6c63d4159686de9d12c5940e513b9c3
paulapol <paula.pol98@yahoo.ro>
2020-08-21 11:14:04 +0300

    change qrcode size

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -5,7 +5,7 @@ module OrdersHelper
     require 'barby/barcode/qr_code'
     require 'barby/outputter/png_outputter'
 
-    barcode = Barby::QrCode.new(text, level: :q, size: 5)
+    barcode = Barby::QrCode.new(text, level: :q, size: 7)
     base64_output = Base64.encode64(barcode.to_png({ xdim: 5 }))
     "data:image/png;base64,#{base64_output}"
   end
